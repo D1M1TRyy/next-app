b3b7c785d3fc7f670947d3902c0f59fd36503465
unknown <duta.shoshiashvili@gmail.com>
2023-11-25 23:07:27 +0400

    second commit

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,24 +1,34 @@
 import Navbar from '@/components/navbar/Navbar'
 import styles from './page.module.css'
+import Image from 'next/image'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       
+      <div className={styles.container}>
+        <div className={styles.wrapper}>
+          <div className={styles.card}>
+            <p>hi</p>
+          </div>
+          <div className={styles.card}>
+            <p>hi</p>
+          </div>
+          <div className={styles.card}>
+            <p>hi</p>
+          </div>
+          <div className={styles.card}>
+            <p>hi</p>
+          </div>
+        </div>
+      </div>
       
-      <div className={styles.wrapper}>
-        <div className={styles.card}>
-          <p>hi</p>
+      <div className={styles.login}>
+        <div className={styles.wrapper}>
+          <input type="text" />
+          <Image src="/1.png" width="20" height="20"/>
         </div>
-        <div className={styles.card}>
-          <p>hi</p>
-        </div>
-        <div className={styles.card}>
-          <p>hi</p>
-        </div>
-        <div className={styles.card}>
-          <p>hi</p>
-        </div>
+        
       </div>
       
     </main>
