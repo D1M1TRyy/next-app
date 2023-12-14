34f2bf5719af425bb9ee1105a4966a9525e943f4
unknown <duta.shoshiashvili@gmail.com>
2023-12-14 13:20:22 +0400

    categori and darkmode development

--- src/components/darkModeToggle/DarkModeToggle.jsx
+++ src/components/darkModeToggle/DarkModeToggle.jsx
@@ -1,9 +1,15 @@
+"use client"
+
+
 import React from 'react'
 import styles from './darkModeToggle.module.css'
 
 function DarkModeToggle() {
+
+  console.log("render");
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} onClick={() => console.log("hello")}>
       <div className={styles.icon}>🌙</div>
       <div className={styles.icon}>🔆</div>
       <div className={styles.ball} />
